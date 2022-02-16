67a937d62d77a167fa81eb4a63e3943684da566a
Yujin Ahn <yujini.ahnn@gmail.com>
2022-02-17 02:50:30 +0900

    [ADD] Posts 수정/조회 기능 추가

---
+++ src/main/java/com/jini/lecture/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,20 @@
+package com.jini.lecture.springboot.web.dto;
+
+import com.jini.lecture.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
