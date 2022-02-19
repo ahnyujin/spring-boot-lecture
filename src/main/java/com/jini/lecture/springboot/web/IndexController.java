e4b4ef5e08ec1231f30921935fc5e67608fab9cb
Yujin Ahn <yujini.ahnn@gmail.com>
2022-02-19 23:42:59 +0900

    [ADD] Add Google Auth Login

--- src/main/java/com/jini/lecture/springboot/web/IndexController.java
+++ src/main/java/com/jini/lecture/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.jini.lecture.springboot.web;
 
+import com.jini.lecture.springboot.config.auth.dto.SessionUser;
 import com.jini.lecture.springboot.service.PostsService;
 import com.jini.lecture.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -24,6 +32,7 @@ public class IndexController {
     public String postsSave() {
         return "posts-save";
     }
+
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
